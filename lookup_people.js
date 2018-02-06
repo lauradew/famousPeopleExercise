1ebc8f9d74344c2b9f95ef32f76951e9553b5af0
Laura De Witt <lauramdewitt@gmail.com>
2018-02-06 20:56:30 +0000

    styled function to match example

--- lookup_people.js
+++ lookup_people.js
@@ -20,7 +20,17 @@ client.connect((err) => {
     if (err) {
       return console.error("error running query", err);
     }
-    console.log(result.rows[0]);
+    console.log("Searching ...");
+    console.log("Found " + result.rows.length + " person(s) by the name '" + dbQuery + "':");
+    for (const row of result.rows) {
+    const dateObj = row.birthdate;
+    const month = dateObj.getUTCMonth() + 1;
+    const day = dateObj.getUTCDate();
+    const year = dateObj.getUTCFullYear();
+    const newDate = "'" + year + "-" + month + "-" + day + "'";
+    const output = "- " + row.id + ": " + row.first_name + " " + row.last_name + ", born " + newDate;
+    console.log(output);
+    }
     client.end();
   });
 });
